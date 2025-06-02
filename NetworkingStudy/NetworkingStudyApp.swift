5270be48079c870496f02511cdd2c62b7a072562
trianglejerry <trianglejerry00@gmail.com>
2025-06-02 17:06:53 +0900

    :tada: git clone

---
+++ NetworkingStudy/NetworkingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingStudyApp.swift
+//  NetworkingStudy
+//
+//  Created by 장은석 on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
